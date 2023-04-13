24d4e46e2ef817283bd13da013960f7767905bdc
Hector Cabra <hectorcabra95@gmail.com>
2023-04-13 22:43:46 +1000

    Updates

---
+++ precache-manifest.ea7cc5b261a37631a857b8c5b4a41e33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7222a9b0134d01becf1",
+    "url": "/biotetris-react/static/css/main.8a18572c.chunk.css"
+  },
+  {
+    "revision": "b7222a9b0134d01becf1",
+    "url": "/biotetris-react/static/js/main.038bed85.chunk.js"
+  },
+  {
+    "revision": "f070d61c50babc718fc7",
+    "url": "/biotetris-react/static/js/runtime~main.3467950b.js"
+  },
+  {
+    "revision": "d0f47f135bf41dca943e",
+    "url": "/biotetris-react/static/js/2.7a3924db.chunk.js"
+  },
+  {
+    "revision": "ba517ca18252c542fb77e704bb1c1684",
+    "url": "/biotetris-react/static/media/bg.ba517ca1.png"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/biotetris-react/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "c762dede3b2844c911fda9b9b39cbde2",
+    "url": "/biotetris-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/biotetris-react/precache-manifest.ea7cc5b261a37631a857b8c5b4a41e33.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/biotetris-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
